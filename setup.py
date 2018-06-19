fbb875f02cee6c001ba8c746398da6eb77444807
Joerg Franke <joerg-franke@outlook.com>
2018-06-19 23:33:07 +0200

    update requirements, add gpu requirements and add  setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# Copyright 2018 Jörg Franke
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import shutil
+from distutils.core import setup
+from setuptools import find_packages
+
+req_file = "requirements.txt" if shutil.which("nvidia-smi") is None else "requirements-gpu.txt"
+
+
+def parse_requirements(filename):
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+install_reqs = parse_requirements(req_file)
+
+setup(name='adnc',
+      version='latest',
+
+      packages=find_packages(exclude=['test', 'test.*']),
+      install_requires=install_reqs,
+
+      dependency_links=[],
+      )
