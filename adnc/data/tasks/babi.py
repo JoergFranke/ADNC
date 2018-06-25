b5fb34aa9027095c36625a0fe36902601a0a6f61
Joerg Franke <joerg-franke@outlook.com>
2018-06-25 10:13:23 +0200

    update babi

--- adnc/data/tasks/babi.py
+++ adnc/data/tasks/babi.py
@@ -19,7 +19,7 @@ import tarfile
 import pathlib
 from collections import Counter
 
-DEFAULT_DATA_FOLDER = ".babi_data"
+DEFAULT_DATA_FOLDER = "data_babi"
 LONGEST_SAMPLE_LENGTH = 1920
 bAbI_URL = 'http://www.thespermwhale.com/jaseweston/babi/tasks_1-20_v1-2.tar.gz'
 
