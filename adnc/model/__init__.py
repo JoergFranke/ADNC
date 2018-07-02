d8602b40b2f5dd6eb938e2748608d0bc651c990c
Joerg Franke <joerg-franke@outlook.com>
2018-07-02 23:06:46 +0200

    update inits

--- adnc/model/__init__.py
+++ adnc/model/__init__.py
@@ -12,3 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+from .mann import MANN
+from .supporter import Supporter
+from .optimizer import Optimizer
