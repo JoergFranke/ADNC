9d9bfd8f7548681c263ba1c177998a28578f5367
Joerg Franke <joerg-franke@outlook.com>
2018-07-04 23:59:24 +0200

    add inference scripts

--- scripts/inference_babi_task.py
+++ scripts/inference_babi_task.py
@@ -13,7 +13,9 @@
 # limitations under the License.
 # ==============================================================================
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="" #gpu not required for inference
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""  # gpu not required for inference
+import argparse
 import yaml
 import numpy as np
 import tensorflow as tf
@@ -21,19 +23,25 @@ import tensorflow as tf
 from adnc.data.loader import DataLoader
 from adnc.model.mann import MANN
 
+parser = argparse.ArgumentParser(description='Load model')
+parser.add_argument('model', type=str, default=False, help='model name')
+model_name = parser.parse_args().model
 
 # Choose a pre trained model by uncomment
-
-# expt_dir = "experiments/pre_trained/babi_task_1/dnc"              # DNC trained on bAbI tasks 1
-# expt_dir = "experiments/pre_trained/babi_task_1/adnc"             # ADNC trained on bAbI tasks 1
-# expt_dir = "experiments/pre_trained/babi_task_1/biadnc"           # BiADNC trained on bAbI tasks 1
-# expt_dir = "experiments/pre_trained/babi_task_all/biadnc"         # BiADNC trained on all bAbI tasks
-expt_dir = "experiments/pre_trained/babi_task_all/biadnc_aug16"     # BiADNC trained on all bAbI tasks with task 16 augmentation
-
+if model_name == 'dnc':
+    expt_dir = "experiments/pre_trained/babi_task_1/dnc"  # DNC trained on bAbI tasks 1
+elif model_name == 'adnc':
+    expt_dir = "experiments/pre_trained/babi_task_1/adnc"  # ADNC trained on bAbI tasks 1
+elif model_name == 'biadnc':
+    expt_dir = "experiments/pre_trained/babi_task_1/biadnc"  # BiADNC trained on bAbI tasks 1
+elif model_name == 'biadnc-all':
+    expt_dir = "experiments/pre_trained/babi_task_all/biadnc"  # BiADNC trained on all bAbI tasks
+else:
+    expt_dir = "experiments/pre_trained/babi_task_all/biadnc_aug16"  # BiADNC trained on all bAbI tasks with task 16 augmentation
 
 config_file = 'config.yml'
-with open(os.path.join(expt_dir, config_file) , 'r') as f:
-    configs = yaml.load(f) # load config from file
+with open(os.path.join(expt_dir, config_file), 'r') as f:
+    configs = yaml.load(f)  # load config from file
 
 dataset_config = configs['babi_task']
 model_config = configs['mann']
@@ -41,27 +49,27 @@ model_config = configs['mann']
 dataset_config['batch_size'] = 1
 model_config['batch_size'] = 1
 
-dataset_config['threads'] = 1 # only one thread for data loading
-dataset_config['max_len'] = 1921 # set max length to maximal
-dataset_config['augment16'] = False # disable augmentation for inference
+dataset_config['threads'] = 1  # only one thread for data loading
+dataset_config['max_len'] = 1921  # set max length to maximal
+dataset_config['augment16'] = False  # disable augmentation for inference
 
 if dataset_config['task_selection'] == ['all']:
-    task_list = [i+1 for i in range(20)]
+    task_list = [i + 1 for i in range(20)]
 else:
     task_list = [int(i) for i in dataset_config['task_selection']]
 
-dl = DataLoader(dataset_config) # load data loader by config
+dl = DataLoader(dataset_config)  # load data loader by config
 
-model_config['input_size'] = dl.x_size # add data size to model config
+model_config['input_size'] = dl.x_size  # add data size to model config
 model_config['output_size'] = dl.y_size
-model_config['memory_unit_config']['bypass_dropout'] = False # no dropout during inference
+model_config['memory_unit_config']['bypass_dropout'] = False  # no dropout during inference
 
-model = MANN(model_config) # load memory augmented neural network  model
+model = MANN(model_config)  # load memory augmented neural network  model
 
-data, target, mask = model.feed # create data feed for session run
+data, target, mask = model.feed  # create data feed for session run
 
-word_dict = dl.dataset.word_dict # full dictionary of all tasks
-re_word_dict = dl.dataset.re_word_dict # reverse dictionary
+word_dict = dl.dataset.word_dict  # full dictionary of all tasks
+re_word_dict = dl.dataset.re_word_dict  # reverse dictionary
 
 print("vocabulary size: {}".format(dl.vocabulary_size))
 print("train set length: {}".format(dl.sample_amount('train')))
@@ -75,7 +83,6 @@ conf.gpu_options.allocator_type = 'BFC'
 conf.gpu_options.allow_growth = True
 
 with tf.Session(config=conf) as sess:
-
     saver.restore(sess, os.path.join(expt_dir, "model_dump.ckpt"))
     mean_error = []
     for task in task_list:
@@ -91,9 +98,9 @@ with tf.Session(config=conf) as sess:
         # infer model
         for v in range(int(dl.batch_amount('test'))):
             sample = next(valid_loader)
-            prediction = sess.run([model.prediction, ],feed_dict={data: sample['x'],
-                                                                  target: sample['y'],
-                                                                  mask: sample['m']})
+            prediction = sess.run([model.prediction, ], feed_dict={data: sample['x'],
+                                                                   target: sample['y'],
+                                                                   mask: sample['m']})
             predictions.append(prediction)
             targets.append(sample['y'])
             masks.append(sample['m'])
@@ -106,10 +113,8 @@ with tf.Session(config=conf) as sess:
             all_corrects += np.sum(corrects * m)
             all_overall += np.sum(m)
 
-        word_error_rate = 1- (all_corrects/all_overall)
+        word_error_rate = 1 - (all_corrects / all_overall)
         mean_error.append(word_error_rate)
 
         print("word error rate task {:2}: {:0.3}".format(task, word_error_rate))
     print("mean word error rate   : {:0.3}".format(np.mean(mean_error)))
-
-
--- scripts/inference_cnn_task.py
+++ scripts/inference_cnn_task.py
@@ -21,26 +21,16 @@ import tensorflow as tf
 from adnc.data.loader import DataLoader
 from adnc.model.mann import MANN
 
-tf.reset_default_graph()
-parser = argparse.ArgumentParser(description='Load model')
-parser.add_argument('model', type=str, default=False, help='model name')
-model_name = parser.parse_args().model
-
-
-if model_name == 'adnc':
-    expt_dir = "experiments/pre_trained/cnn_rc_task/adnc"
-else:
-    expt_dir = "experiments/pre_trained/cnn_rc_task/biadnc"
-
+expt_dir = "experiments/pre_trained/cnn_rc_task/adnc"
 
 # load config file
-with open(os.path.join(expt_dir, 'config.yml') , 'r') as f:
+with open(os.path.join(expt_dir, 'config.yml'), 'r') as f:
     configs = yaml.load(f)
 data_set_config = configs['cnn']
 model_config = configs['mann']
 
 # set batch size to 1 for inference and max_len to maximal value
-BATCH_SIZE =1
+BATCH_SIZE = 1
 data_set_config['batch_size'] = BATCH_SIZE
 data_set_config['max_len'] = 20000
 model_config['batch_size'] = BATCH_SIZE
@@ -55,11 +45,10 @@ dl.dataset.load_dictionary(expt_dir)
 model_config['input_size'] = dl.x_size
 model_config['output_size'] = dl.y_size
 model_config['memory_unit_config']['bypass_dropout'] = False
-model_config['input_embedding'] = {'word_idx_dict': dl.dataset.word_idx_dict, 'embedding_size':100}
+model_config['input_embedding'] = {'word_idx_dict': dl.dataset.word_idx_dict, 'embedding_size': 100}
 
 # init MANN model
 model = MANN(model_config)
-
 
 # print model/data values
 print("vocabulary size: {}".format(dl.vocabulary_size))
@@ -70,18 +59,17 @@ print("valid batch amount: {}".format(dl.batch_amount('valid')))
 print("model parameter amount: {}".format(model.parameter_amount))
 print("x size: {}".format(dl.y_size))
 
-
 # calculate error
 data = model._data
 answer_idx = tf.placeholder(tf.int64, [data_set_config['batch_size']], name='labels')
 candidates_mask = tf.placeholder(tf.float32, [data_set_config['batch_size'], dl.y_size], name='candidates')
 outputs = model.outputs
-last_output=outputs[-1,:,:]
+last_output = outputs[-1, :, :]
 masked_output = candidates_mask * last_output
 predictions = tf.nn.softmax(masked_output, dim=-1)
-arg_predictions = tf.arg_max(predictions,dimension=-1)
+arg_predictions = tf.arg_max(predictions, dimension=-1)
 equal = tf.cast(tf.equal(arg_predictions, answer_idx), tf.float32)
-error_rate = tf.reduce_mean( equal )
+error_rate = tf.reduce_mean(equal)
 
 # init saver
 saver = tf.train.Saver()
@@ -92,16 +80,16 @@ conf.gpu_options.allocator_type = 'BFC'
 conf.gpu_options.allow_growth = True
 
 with tf.Session(config=conf) as sess:
-
     saver.restore(sess, os.path.join(expt_dir, "model_dump.ckpt"))
 
     # calculate validation error
     valid_cost = 0
     valid_error = 0
     valid_count = 0
-    for _ in tqdm(range( int(dl.batch_amount('valid')))):
+    for _ in tqdm(range(int(dl.batch_amount('valid')))):
         sample = next(valid_loader)
-        verror = sess.run([error_rate],feed_dict={data: sample['x'], answer_idx:sample['answer_idx'], candidates_mask:sample['candidates']})
+        verror = sess.run([error_rate], feed_dict={data: sample['x'], answer_idx: sample['answer_idx'],
+                                                   candidates_mask: sample['candidates']})
         valid_error += verror
         valid_count += 1
     valid_error = valid_error / valid_count
@@ -111,11 +99,11 @@ with tf.Session(config=conf) as sess:
     test_cost = 0
     test_error = 0
     test_count = 0
-    for _ in tqdm(range( int(dl.batch_amount('test')))):
+    for _ in tqdm(range(int(dl.batch_amount('test')))):
         sample = next(test_loader)
-        terror = sess.run([error_rate],feed_dict={data: sample['x'], answer_idx:sample['answer_idx'], candidates_mask:sample['candidates']})
+        terror = sess.run([error_rate], feed_dict={data: sample['x'], answer_idx: sample['answer_idx'],
+                                                   candidates_mask: sample['candidates']})
         test_error += terror
         test_count += 1
     test_error = test_error / test_count
     print("test: tf mean acc: {}".format(test_error))
-
